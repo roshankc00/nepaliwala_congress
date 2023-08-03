f0f4a2f5bd2c9a5b60b368e431013c8255d4c6ed
roshankc00 <120471898+roshankc00@users.noreply.github.com>
2023-08-03 21:23:04 +0545

    feat:done

--- src/components/Upperheader/index.jsx
+++ src/components/Upperheader/index.jsx
@@ -4,7 +4,7 @@ const UpperNav = () => {
   return (
     <div className="">
         <div className="upper-navbar d-flex justify-content-between align-items-center shadow-sm flex-wrap">
-            <img src="public/images/logo.svg" className="upper-navbar-logo p-2 ms-2" alt="loading" />
+            <img src="/images/logo.svg" className="upper-navbar-logo p-2 ms-2" alt="loading" />
             <div className="buttons d-flex gap-3 mx-2 mx-5 ">
                 <button className='btn btn-outline-secondary'>English</button>
                 <button className='btn btn-outline-secondary'>nepali</button>
